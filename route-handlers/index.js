fff7d19babbf036f3573449d5b323e6477105ec7
zacharygagnier <gzachary@gmail.com>
2017-11-27 16:52:58 -0600

    (refactor) app.use to loop over paths, index now lists all

---
+++ route-handlers/index.js
@@ -0,0 +1,6 @@
+exports.favorite = require('./db-favorite')
+exports.rating = require('./db-rating')
+exports.route = require('./db-route')
+exports.session = require('./db-session')
+exports.user = require('./db-user')
+exports.waypoint = require('./db-waypoint')
