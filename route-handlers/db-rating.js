3586702d76eaee3b734957038320bda02c4dce2a
zacharygagnier <gzachary@gmail.com>
2017-11-28 11:56:35 -0600

    (chore) commiting for fix

--- route-handlers/db-rating.js
+++ route-handlers/db-rating.js
@@ -13,18 +13,6 @@ app.get(`/${path}`, (req, res) => {
 
 app.post(`/${path}`, (req, res) => {
 
-    // knex.transaction(trx => {
-    //     trx(path).where('username', name).then(res => {
-    //       if (res.length === 0) {
-    //         return trx('users').insert({ username: name }).then(() => {
-    //           return trx('users').where('username', name);
-    //         });
-    //       } else {
-    //         return res;
-    //       }
-    //     });
-    //   }).then(res => console.log(`User is: ${res[0]}`));
-    
 });
 
 app.put(`/${path}`, (req, res) => {
