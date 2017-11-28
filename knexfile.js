ca4754daba1f605cb2d8ef2c0236ea187e68187e
zacharygagnier <gzachary@gmail.com>
2017-11-27 18:10:53 -0600

    (clean) commented out code

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,6 @@ require('dotenv').config()
 const {DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DATABASE} = process.env;
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
   development: {
     client: 'pg',
     connection: {
@@ -19,46 +12,5 @@ module.exports = {
       password : DB_PASS,
       database : DB_DATABASE
     },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   directory: './migrations'
-    //   tableName: 'knex_migrations'
-    // }
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
 };
