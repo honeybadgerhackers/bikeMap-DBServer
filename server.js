f5406d8f4cb0b8450eb3b063bf0f52bebb2db722
zacharygagnier <gzachary@gmail.com>
2017-11-28 14:33:45 -0600

    (cleanup) cleaned spaces in migration file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const db = require('./db')
-const paths = require('./route-handlers')
+const db = require('./db');
+const paths = require('./route-handlers');
 
 const app = express();
 
