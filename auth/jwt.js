10132260d2a587a47e87fc31dfe97a06d7a72771
Rob Aird <robkaird@gmail.com>
2017-12-01 12:29:20 -0600

    (feat) basic implementation of functions for json web tokens

---
+++ auth/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+const { SECRET, AUDIENCE, ISSUER } = process.env;
+
+const genJti = () => {
+  let jti = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  for (let i = 0; i < 16; i += 1) {
+    jti += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return jti;
+};
+
+exports.createIdToken = user => (
+  jwt.sign(_.omit(user, 'access_token'), SECRET, { expiresIn: '10h' })
+);
+
+exports.createAccessToken = () => (
+  jwt.sign({
+    iss: ISSUER,
+    aud: AUDIENCE,
+    exp: '10h',
+    jti: genJti(),
+    scope: 'full_access',
+    alg: 'HS256',
+  }, SECRET)
+);
+
