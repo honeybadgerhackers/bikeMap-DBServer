03173672f0555a9e9e54eea426ecc4a0a6c8badc
Rob Aird <robkaird@gmail.com>
2017-12-10 15:19:52 -0600

    (add) call to Google API to create image

--- utilities/google.js
+++ utilities/google.js
@@ -4,7 +4,17 @@ const { GOOGLE_API_KEY } = process.env;
 
 module.exports = {
   reverseGeocode: coords => (
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords}&key=${GOOGLE_API_KEY}`)
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?
+      latlng=${coords}
+      &key=${GOOGLE_API_KEY}`)
+      .then(result => result)
+      .catch(err => err)
+  ),
+  getRouteImage: coords => (
+    axios.get(`https://maps.googleapis.com/maps/api/staticmap?
+      size=600x600
+      &path=weight:4|color:red|enc:${coords}
+      &key=${GOOGLE_API_KEY}`)
       .then(result => result)
       .catch(err => err)
   ),
