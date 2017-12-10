5c81e757b872749933b0180a3e0c111191e7ea44
Rob Aird <robkaird@gmail.com>
2017-12-10 14:47:51 -0600

    (add) migrations to add user data, save polylines

--- migrations/20171201684720_bike_app.js
+++ migrations/20171201684720_bike_app.js
@@ -3,7 +3,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('user_account', (t) => {
-      t.string('social_media_id').unique();;
+      t.string('social_media_id').unique();
       t.string('social_media_token');
     }),
   ]);
--- migrations/20171205123828_user_stats.js
+++ migrations/20171205123828_user_stats.js
@@ -1,11 +1,30 @@
 // * Add stats to user_account
 exports.up = (knex, Promise) => {
-  // Calories burned
-  // Distance travelled
-  // Average speed
-  // Most travelled route
+  return Promise.all([
+    knex.schema.table('route', (t) => {
+      t.string('polyline_forward');
+      t.string('polyline_backward');
+    }),
+    knex.schema.table('user_account', (t) => {
+      t.number('calories');
+      t.number('avg_speed');
+      t.number('avg_speed_counter');
+      t.number('total_distance');
+    }),
+  ]);
 };
 
 exports.down = (knex, Promise) => {
-  
+  return Promise.all([
+    knex.schema.table('route', (t) => {
+      t.dropColumn('polyline_forward');
+      t.dropColumn('polyline_backward');
+    }),
+    knex.schema.table('user_account', (t) => {
+      t.dropColumn('calories');
+      t.dropColumn('avg_speed');
+      t.dropColumn('avg_speed_counter');
+      t.dropColumn('total_distance');
+    }),
+  ]);
 };
