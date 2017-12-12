5c0b4244f5875d5408348f1186e671fa33a9c35b
Zachary Gagnier <gagnierzachary@gmail.com>
2017-12-11 19:50:42 -0600

    (refactor) get routenearby to have a variable distance

--- route-handlers/db-route.js
+++ route-handlers/db-route.js
@@ -48,19 +48,23 @@ app.get(`/${path}`, (req, res) => {
 });
 
 app.get(`/${path}nearby`, (req, res) => {
-  const filter = req.headers.filter ? JSON.parse(req.headers.filter) : {};
+  const filter = req.headers.filter ? JSON.parse(req.headers.filter) : {lat: 29.9459695, lng: -90.07005989999999};
+  if (!filter.distance) {
+    filter.distance = 0.07
+  };
+  const {lat, lng, distance} = filter;
   knex('waypoint')
     .select()
     .where(function() {
       this.where({count: 0})
-      .whereBetween('lat', [filter.lat - .09, filter.lat + .09])
-      .andWhereBetween('lng', [filter.lng - .09, filter.lat + .09])
+      .whereBetween('lat', [lat - distance, lat + distance])
+      .andWhereBetween('lng', [lng - distance, lng + distance])
     })
     .orWhere(function() {
         this.whereNot({count: 0})
         .andWhereNot({street: null})
-        .whereBetween('lat', [filter.lat - .09, filter.lat + .09])
-        .andWhereBetween('lng', [filter.lng - .09, filter.lat + .09])
+        .whereBetween('lat', [lat - distance, lat + distance])
+        .andWhereBetween('lng', [lng - distance, lng + distance])
     })
     .join('route', 'route.id', '=', 'waypoint.id_route')
     .then((results) => {
