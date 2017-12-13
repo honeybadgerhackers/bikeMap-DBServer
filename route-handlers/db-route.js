dd83caa1db53d76908a8f8c3f62e029447784995
Patrick Brown Jr <pjbrn26@gmail.com>
2017-12-13 13:22:21 -0600

    No Changes

--- route-handlers/db-route.js
+++ route-handlers/db-route.js
@@ -14,8 +14,29 @@ app.get(`/${path}`, (req, res) => {
     .select()
     .then((results) => {
       res.send(results);
-    });
+    }); 
 });
+
+app.get(`/${path}&location`, (req, res) => {
+  const filter = req.headers.filter ? JSON.parse(req.headers.filter) : {};
+  knex(path)
+    .where(filter)
+    .join('waypoint', 'route.id', '=', 'waypoint.id_route')
+    .select('route.id', 'route.route_name', 'route.type', 'route.current_rating', 
+            'route.favorite_count', 'waypoint.lat', 'waypoint.lng', 'waypoint.count')
+    .then((results) => {
+      let waypoints = results.map(result => {
+        return {lat: result.lat, lng: result.lng, count: result.count}
+      });
+      delete results[0].lat;
+      delete results[0].lng;
+      results[0].waypoints = waypoints;
+      let mergedRoute = results[0];
+      res.send(mergedRoute);
+    }).catch((error) => {
+      console.log(error);
+    })      
+}); 
 
 app.post(`/${path}`, (req, res) => {
   knex(path)
